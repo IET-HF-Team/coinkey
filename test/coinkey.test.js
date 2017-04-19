957dfde6f544efe0f3ea8e7556f5b8c38a603724
marcigo36 <marcigo36@users.noreply.github.com>
2017-04-19 16:23:35 +0200

    additional code style fixes in tests #1

--- test/coinkey.test.js
+++ test/coinkey.test.js
@@ -14,35 +14,35 @@ describe("CoinKey", function () {
         var ck = new CoinKey(privateKey);
         assert(ck.compressed);
         assert.equal(ck.privateKey.toString("hex"), privateKey.toString("hex"));
-      })
-    })
+      });
+    });
 
     describe("> when private key and versions", function () {
       it("should return an instance of CoinKey with versions", function () {
-        var dogecoin = fixtures.valid.filter(function (f) { if (f.description.match(/dogecoin/)) return f})[0];
+        var dogecoin = fixtures.valid.filter(function (f) { if (f.description.match(/dogecoin/)){ return f; }})[0];
         var ck = new CoinKey(new Buffer(dogecoin.privateKey, 'hex'), dogecoin.versions);
         assert(ck.compressed);
-      })
-    })
+      });
+    });
 
     describe("> when no private key", function () {
       it("should throw an error", function () {
         /* eslint-disable no-new */
-        assert.throws(function () { new CoinKey() }, /must be arrayish/);
-        assert.throws(function () { new CoinKey({public: 0, private: 0x80}) }, /must be arrayish/);
+        assert.throws(function () { new CoinKey(); }, /must be arrayish/);
+        assert.throws(function () { new CoinKey({public: 0, private: 0x80}); }, /must be arrayish/);
         /* eslint-enable no-new */
-      })
-    })
+      });
+    });
 
     describe("> when a coininfo object is passed for versions", function () {
       it("should return the proper address / WIF", function () {
-        var dogecoin = fixtures.valid.filter(function (f) { if (f.description.match(/dogecoin/)) return f})[0];
-        var ck = new CoinKey(new Buffer(dogecoin.privateKey, 'hex'), coininfo('DOGE'));
+        var dogecoin = fixtures.valid.filter(function (f) { if (f.description.match(/dogecoin/)){ return f;}})[0];
+        var ck = new CoinKey(new Buffer(dogecoin.privateKey, "hex"), coininfo("DOGE"));
         assert.equal(ck.privateWif, dogecoin.privateWifCompressed);
         assert.equal(ck.publicAddress, dogecoin.publicAddressCompressed);
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe("- privateWif", function () {
     fixtures.valid.forEach(function (f) {
@@ -52,9 +52,9 @@ describe("CoinKey", function () {
         assert.equal(ck.privateWif, f.privateWif);
         var ckCompressed = new CoinKey(new Buffer(f.privateKey, "hex"), f.versions);
         assert.equal(ckCompressed.privateWif, f.privateWifCompressed);
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe("- publicAddress", function () {
     fixtures.valid.forEach(function (f) {
@@ -64,9 +64,9 @@ describe("CoinKey", function () {
         assert.equal(ck.publicAddress, f.publicAddress);
         var ckCompressed = new CoinKey(new Buffer(f.privateKey, "hex"), f.versions);
         assert.equal(ckCompressed.publicAddress, f.publicAddressCompressed);
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe("- toString()", function () {
     fixtures.valid.forEach(function (f) {
@@ -76,9 +76,9 @@ describe("CoinKey", function () {
         assert.equal(ck.toString(), f.privateWif + ": " + f.publicAddress);
         var ckCompressed = new CoinKey(new Buffer(f.privateKey, "hex"), f.versions);
         assert.equal(ckCompressed.toString(), f.privateWifCompressed + ": " + f.publicAddressCompressed);
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe("+ fromWif()", function () {
     fixtures.valid.forEach(function (f) {
@@ -94,17 +94,17 @@ describe("CoinKey", function () {
         assert.equal(ckCompressed.compressed, true);
         assert.equal(ckCompressed.versions.private, f.versions.private);
         assert.equal(ckCompressed.versions.public, f.versions.public);
-        assert.equal(ckCompressed.privateKey.toString('hex'), f.privateKey);
+        assert.equal(ckCompressed.privateKey.toString("hex"), f.privateKey);
         assert.equal(ckCompressed.publicAddress, f.publicAddressCompressed);
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe("- versions", function () {
     describe("> when object changes", function () {
       it("should change the wif and public address", function () {
-        var bitcoin = fixtures.valid.filter(function (f) { if (f.description.match(/bitcoin/)) return f })[0];
-        var dogecoin = fixtures.valid.filter(function (f) { if (f.description.match(/bitcoin/)) return f })[0];
+        var bitcoin = fixtures.valid.filter(function (f) { if (f.description.match(/bitcoin/)){ return f; }})[0];
+        var dogecoin = fixtures.valid.filter(function (f) { if (f.description.match(/bitcoin/)){ return f; }})[0];
 
         var ck = new CoinKey(new Buffer(bitcoin.privateKey, "hex"));
         assert.equal(ck.privateWif, bitcoin.privateWifCompressed);
@@ -115,13 +115,13 @@ describe("CoinKey", function () {
         assert.equal(ck.privateWif, dogecoin.privateWifCompressed);
         assert.equal(ck.publicAddress, dogecoin.publicAddressCompressed);
 
-      })
-    })
+      });
+    });
 
     describe("> when field changes", function () {
       it("should change the wif and public address", function () {
-        var bitcoin = fixtures.valid.filter(function (f) { if (f.description.match(/bitcoin/)) return f })[0];
-        var dogecoin = fixtures.valid.filter(function (f) { if (f.description.match(/bitcoin/)) return f })[0];
+        var bitcoin = fixtures.valid.filter(function (f) { if (f.description.match(/bitcoin/)){ return f; } })[0];
+        var dogecoin = fixtures.valid.filter(function (f) { if (f.description.match(/bitcoin/)){ return f; } })[0];
 
         var ck = new CoinKey(new Buffer(bitcoin.privateKey, "hex"));
         assert.equal(ck.privateWif, bitcoin.privateWifCompressed);
@@ -133,9 +133,9 @@ describe("CoinKey", function () {
 
         assert.equal(ck.privateWif, dogecoin.privateWifCompressed);
         assert.equal(ck.publicAddress, dogecoin.publicAddressCompressed);
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe("+ createRandom()", function () {
     describe("> when no versions", function () {
@@ -143,26 +143,26 @@ describe("CoinKey", function () {
         var ck = CoinKey.createRandom();
         assert(ck.compressed);
         assert(ck.privateKey);
-      })
-    })
+      });
+    });
 
     describe("> when versions", function () {
       it("should create a random CoinKey with versions specified", function () {
-        var dogecoin = fixtures.valid.filter(function (f) { if (f.description.match(/bitcoin/)) return f })[0];
+        var dogecoin = fixtures.valid.filter(function (f) { if (f.description.match(/bitcoin/)){ return f;} })[0];
         var ck = CoinKey.createRandom(dogecoin.versions);
         assert(ck.compressed);
         assert(ck.privateKey);
         assert.equal(ck.versions.private, dogecoin.versions.private);
         assert.equal(ck.versions.public, dogecoin.versions.public);
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe("+ addressToHash()", function () {
     it("should convert an address to the hash", function () {
       assert.equal(CoinKey.addressToHash("1FkKMsKNJqWSDvTvETqcCeHcUQQ64kSC6s").toString("hex"), "a1c2f92a9dacbd2991c3897724a93f338e44bdc1");
       assert.equal(CoinKey.addressToHash("NBKgZWpMEDbzkTiRWHABRASXCdo8zWM8qC").toString("hex"), "a1c2f92a9dacbd2991c3897724a93f338e44bdc1");
       assert.equal(CoinKey.addressToHash("3NukJ6fYZJ5Kk8bPjycAnruZkE5Q7UW7i8").toString("hex"), "e8c300c87986efa84c37c0519929019ef86eb5b4");
-    })
-  })
-})
+    });
+  });
+});
